689599aefda6099548af8988cb41bc291800b29c
Dömötör Gulyás <dognotdog@gmail.com>
2021-09-21 12:58:43 -0400

    add documentation generation via ExDoc

--- mix.exs
+++ mix.exs
@@ -7,7 +7,15 @@ defmodule Spex.MixProject do
       version: "0.1.0-dev",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # docs
+      name: "Spex",
+      source_url: "https://github.com/CMU-CREATE-Lab/spex",
+      docs: [
+        main: "Spex",
+        extras: ["README.md"],
+        ],
     ]
   end
 
@@ -30,7 +38,11 @@ defmodule Spex.MixProject do
       {:jason, "~> 1.2"},
       {:xlsxir, "~> 1.6.4"},
       # time zones
-      {:tzdata, "~> 1.1"}
+      {:tzdata, "~> 1.1"},
+
+      # generate documentation
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false},
+
 
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
