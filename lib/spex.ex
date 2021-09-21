a1de67bc3beb369887f1f50b4898976890531a73
Dömötör Gulyás <dognotdog@gmail.com>
2021-09-21 13:05:55 -0400

    update moduledoc on main module

--- lib/spex.ex
+++ lib/spex.ex
@@ -1,18 +1,16 @@
 defmodule Spex do
   @moduledoc """
-  `Spex` is a collection of tools for working with spectroscopy data.
+  `Spex` is a collection of tools for working with spectroscopy data. 
+
+  It contains utilities for loading and working with reference spectra in `Spex.RefSpec`.
+
+  `Spex.WebSpec` is for downloading spectrometry data.
+
+  `Spex.Utils` provides various helpers for data wrangling and analysis
+
+  `Spex.Regression` implements linear regression for mapping spectrometry data to known compounds.
+
+  `Spex.Interp` provides functions to re-map data series with sample periods.
+
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Spex.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
