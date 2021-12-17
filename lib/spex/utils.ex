7a30ef7155eb8e91847ac87572b01c061c4b110d
Dömötör Gulyás <dognotdog@gmail.com>
2021-12-17 13:02:05 -0500

    adds combined slope then value regression method, which seems more promising to create stable signal reconstruction

--- lib/spex/utils.ex
+++ lib/spex/utils.ex
@@ -44,7 +44,7 @@ defmodule Spex.Utils do
   """
   def slope_regression(signal, ref_signal, wl, regressors, opts \\ []) do
     wlmin = Keyword.get(opts, :wlmin, 220)
-    wlmax = Keyword.get(opts, :wlmax, 360)
+    wlmax = Keyword.get(opts, :wlmax, 310)
     imax = Enum.count(wl, fn x -> x < wlmax end)
     imin = Enum.count(wl, fn x -> x < wlmin end)
     regd_range = imin..(imax-2)
@@ -55,7 +55,7 @@ defmodule Spex.Utils do
     regd = for(reg <- regressors, do: diff(reg, axis: 1))
     onesd = Nx.tensor([for(_i <- 1..(Enum.count(wl)-1), do: 1.0e-3)])
     xd = Nx.concatenate([onesd | regd], axis: 0) |> Nx.transpose()
-    xd = xd[imin..(imax-2)] 
+    xd = xd[regd_range] 
 
     reg_range = imin..(imax-1)
     ones = Nx.tensor([for(_i <- 1..Enum.count(wl), do: 0.0e-0)])
@@ -67,6 +67,92 @@ defmodule Spex.Utils do
   end
 
   @doc """
+  do a combined slope, then value regression. regressors are of the format `{name, regressor}`.
+  """
+  def slope_value_regression(signal, ref_signal, wl, slope_regressors, value_regressors, opts \\ []) do
+    wlmin = Keyword.get(opts, :wlmin, 220)
+    wlmax = Keyword.get(opts, :wlmax, 310)
+    imax = Enum.count(wl, fn x -> x < wlmax end)
+    imin = Enum.count(wl, fn x -> x < wlmin end)
+    regd_range = imin..(imax-2)
+
+    y = signal_to_attenuation(signal, ref_signal)
+    yd = y |> diff()
+    yd = yd[regd_range]
+    regd = for({_name, reg} <- slope_regressors, do: diff(reg, axis: 1))
+    onesd = Nx.tensor([for(_i <- 1..(Enum.count(wl)-1), do: 1.0e-3)])
+    xd = Nx.concatenate([onesd | regd], axis: 0) |> Nx.transpose()
+    xd = xd[regd_range] 
+
+
+    # first regression runs on the slopes of the signal
+    bd = Spex.Regression.linreg(xd,yd)
+
+    # second regression on value, but after initial model has been subtracted
+    # compute X tensor for signal reconstruction (ignores constant first term of model, hence ones is actually zeroes)
+    reg_range = imin..(imax-1)
+    regs = for({_name, reg} <- slope_regressors, do: reg)
+    ones = Nx.tensor([for(_i <- 1..Enum.count(wl), do: 0.0e-0)])
+    xs = Nx.concatenate([ones | regs], axis: 0) |> Nx.transpose()
+    xs = xs[reg_range] 
+
+    y_regd = Nx.dot(xs, bd)
+    y_slope = Nx.subtract(y, y_regd)
+
+    # recompute X matrix for value regression
+    regs = for({_name, reg} <- value_regressors, do: reg)
+    ones = Nx.tensor([for(_i <- 1..Enum.count(wl), do: 1.0e-0)])
+    xv = Nx.concatenate([ones | regs], axis: 0) |> Nx.transpose()
+    xv = xv[reg_range] 
+
+    # use y_sloped, eg. input signal with slope results subtracted
+    bv = Spex.Regression.linreg(xv,y_slope)
+
+    y_reg = Nx.dot(xv, bv)
+
+    # combine b values across slope and value regressors
+    slope_params = for {{name, _reg}, b} <- Enum.zip(slope_regressors, bd |> Nx.to_flat_list() |> Enum.slice(1..-1//1)) do
+      {name, b}
+    end
+    value_params = for {{name, _reg}, b} <- Enum.zip(value_regressors, bv |> Nx.to_flat_list() |> Enum.slice(1..-1//1)) do
+      {name, b}
+    end
+    all_params = for {name, b} <- slope_params ++ value_params, reduce: %{} do
+      acc ->
+        Map.update(acc, name, b, fn b_old -> b_old + b end)
+    end
+    |> Enum.to_list()
+    |> Enum.sort_by(fn {name, _} -> name end)
+
+    combined_names = for {name, _b} <- all_params, do: name
+
+    # add the ones
+    const_param = {:const, bv |> Nx.to_flat_list() |> Enum.at(0)}
+    all_params = [const_param |  all_params]
+
+
+    # recompute X matrix for combined regression
+    combined_regressors = for name <- combined_names, do: Keyword.get(slope_regressors, name) || Keyword.get(value_regressors, name)
+
+    # IO.puts("combined_regressors=#{inspect(combined_regressors)}")
+
+    ones = Nx.tensor([for(_i <- 1..Enum.count(wl), do: 1.0e-0)])
+    x = Nx.concatenate([ones | combined_regressors], axis: 0) |> Nx.transpose()
+    x = x[reg_range] 
+
+    # IO.puts("x=#{inspect(x)}")
+
+    %{
+      x_slope: xs, x_value: xv,
+      b_slope: bd, b_value: bv, 
+      y_slope: y_slope, y_value: y_reg,
+      combined_x: x,
+      combined_params: all_params,
+    }
+
+  end
+
+  @doc """
   perform a linear regression based on the first Difference-of-Gaussians.
 
   Returns x,b to make it possible to reconstruct the estimated signal.
@@ -74,7 +160,7 @@ defmodule Spex.Utils do
   """
   def dog_regression(signal, ref_signal, wl, regressors, opts \\ []) do
     wlmin = Keyword.get(opts, :wlmin, 220)
-    wlmax = Keyword.get(opts, :wlmax, 360)
+    wlmax = Keyword.get(opts, :wlmax, 310)
     imax = Enum.count(wl, fn x -> x < wlmax end)
     imin = Enum.count(wl, fn x -> x < wlmin end)
     regd_range = imin..(imax-2)
@@ -105,7 +191,7 @@ defmodule Spex.Utils do
   """
   def value_regression(signal, ref_signal, wl, regressors, opts \\ []) do
     wlmin = Keyword.get(opts, :wlmin, 210)
-    wlmax = Keyword.get(opts, :wlmax, 410)
+    wlmax = Keyword.get(opts, :wlmax, 310)
     imax = Enum.count(wl, fn x -> x < wlmax end)
     imin = Enum.count(wl, fn x -> x < wlmin end)
     reg_range = imin..(imax-1)
@@ -120,7 +206,7 @@ defmodule Spex.Utils do
 
   def tls_regression(signal, ref_signal, wl, regressors, opts \\ []) do
     wlmin = Keyword.get(opts, :wlmin, 210)
-    wlmax = Keyword.get(opts, :wlmax, 410)
+    wlmax = Keyword.get(opts, :wlmax, 310)
     imax = Enum.count(wl, fn x -> x < wlmax end)
     imin = Enum.count(wl, fn x -> x < wlmin end)
     reg_range = imin..(imax-1)
